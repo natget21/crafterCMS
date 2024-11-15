a3b372638021de19dcc9512615db3d1b2ee117de
admin admin <evaladmin@example.com>
2024-11-15 02:08:39 +0000

    "User admin wrote content /scripts/rest/course.get.groovy"

--- scripts/rest/course.get.groovy
+++ scripts/rest/course.get.groovy
@@ -20,7 +20,7 @@ def courseList = []
             def subCategories = []
             course.item?.descriptorDom?.component?.subcategory_o?.item?.each { subCategoryItem ->
             def subCategoryPath = subCategoryItem.key?.text
-            def subCategoryFile = siteItemService.getSiteItem(categoryPath)
+            // def subCategoryFile = siteItemService.getSiteItem(categoryPath)
             // def subCategoryDetails = [
             //         name: subCategoryFile.item?.descriptorDom?.component?.subCategoryName_s,
             //         description: subCategoryFile.item?.descriptorDom?.component?.description
