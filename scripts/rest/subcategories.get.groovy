f4dfdbdd43dfe81151bdcba82e5d893bdf9212ee
admin admin <evaladmin@example.com>
2024-11-15 00:55:31 +0000

    "User admin wrote content /scripts/rest/subcategories.get.groovy"

--- scripts/rest/subcategories.get.groovy
+++ scripts/rest/subcategories.get.groovy
@@ -1,2 +1,12 @@
-def subCategoriesItem = siteItemService.getSiteItem("/site/components/sub_categories");
-return subCategoriesItem;
+// def subCategoriesItem = siteItemService.getSiteItem("/site/components/sub_categories");
+// return subCategoriesItem;
+
+def siteTree = siteItemService.getSiteTree("/site/components/sub_categories", -1)
+def subCategoryList = []
+if (siteTree) {
+    def items = siteTree.childItems;
+    items.each { siteItem ->
+        subCategoryList.add(siteItem);
+    }
+    return subCategoryList;
+}
