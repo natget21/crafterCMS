e1da43971a147cbdffb3c1e30350e9b1aa52375d
admin admin <evaladmin@example.com>
2024-11-15 11:48:55 +0000

    "User admin wrote content /scripts/rest/macro_category/structure.get.groovy"

--- scripts/rest/macro_category/structure.get.groovy
+++ scripts/rest/macro_category/structure.get.groovy
@@ -33,7 +33,7 @@
 
 
 def topNavItems = [:]
-def siteDir = siteItemService.getSiteTree("/", 3)
+def siteDir = siteItemService.getSiteTree("/config", 3)
 
 if (siteDir) {
     def dirs = siteDir.childItems
