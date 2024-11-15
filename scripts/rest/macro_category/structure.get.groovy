61f4e8e129d55402f17c9d3e65c56502a609d83f
admin admin <evaladmin@example.com>
2024-11-15 14:26:32 +0000

    "User admin wrote content /scripts/rest/macro_category/structure.get.groovy"

--- scripts/rest/macro_category/structure.get.groovy
+++ scripts/rest/macro_category/structure.get.groovy
@@ -41,7 +41,7 @@ if (siteDir) {
         def dirName = dir.getStoreName()
         def dirItem = siteItemService.getSiteItem("/config/studio/content-types/component/${dirName}/form-definition.xml")
         if (dirItem != null) {
-        def data = dirItem.getChildItems()
+        def data = dirItem.geetDom()
         topNavItems.put(dirName, data)
         }
     }
