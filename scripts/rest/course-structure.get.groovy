3910880db10f617f0fadfe4069b22e1c15ef3e96
admin admin <evaladmin@example.com>
2024-11-15 10:11:39 +0000

    "User admin wrote content /scripts/rest/course-structure.get.groovy"

--- scripts/rest/course-structure.get.groovy
+++ scripts/rest/course-structure.get.groovy
@@ -2,11 +2,11 @@
 
 
     def component = siteItemService.getSiteItem("/site/components/items/course")
-        def xmlContent = component.getAsXml()
-        def fields = []
+        // def xmlContent = component.getAsXml()
+        // def fields = []
 
-        xmlContent.fields.field.each { field ->
-            fields <<field
-        }
+        // xmlContent.fields.field.each { field ->
+        //     fields <<field
+        // }
 
     return component
