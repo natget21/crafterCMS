55baa88a9f8b6b567a34da1333aabbcf816afdc9
admin admin <evaladmin@example.com>
2024-11-15 10:14:47 +0000

    "User admin wrote content /scripts/rest/course-structure.get.groovy"

--- scripts/rest/course-structure.get.groovy
+++ scripts/rest/course-structure.get.groovy
@@ -2,11 +2,5 @@
 
 
 def descriptor = siteItemService.getSiteItem("/site/components/items/course.meta.xml")
-        // def xmlContent = component.getAsXml()
-        // def fields = []
 
-        // xmlContent.fields.field.each { field ->
-        //     fields <<field
-        // }
-
-    return component
+    return descriptor
